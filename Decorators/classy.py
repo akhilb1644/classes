7b41959993cc011718d888edd9a5c5b13e93deb8
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-08-07 21:24:43 -0400

    Create classy.py

---
+++ Decorators/classy.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self,firstName,lastName,birthplace,id):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthplace = birthplace
+        self.id = id
+        
+    def show_id(func):
+        def reg(self,*args,**kwargs):
+            print("\nYour ID has been shown.")
+            func(self,*args,**kwargs)
+        return reg
+    
+    @show_id
+    def take_debt(self,dollars):
+        print(f"A debt of ${dollars} has been taken out.")
+    
+    @show_id
+    def fly_to_paris(self):
+        print("I am flying to Paris.")
+        
+    def print_details(self):
+        print(f"My name is {self.firstName.title()} {self.lastName.title()} and I was born in the {self.birthplace}. My ID is {self.id}.")
